193d9a891a26f4452335233d64e9df40224a19cc
Alun Price <alunprice089@gmail.com>
2023-01-26 20:32:14 +0000

    s6 63 add the age prop

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <p>Hey!!!</p>
+  <h3>Hey!!!</h3>
   <Greeting></Greeting>
+  <user :age="age"></user>
+  <!-- note use of lowercase tags -->
 </template>
 
 <script>
 import Greeting from "@/components/Greeting.vue";
+import User from "@/components/User.vue";
 
 export default {
   name: "App",
   components: {
     Greeting,
+    User,
+  },
+  data() {
+    return {
+      age: 20,
+    };
   },
 };
 </script>
